b8863f38fe558e16091149c980a2c405faa5982e
Miro (Mehdi) <valjangdev@gmail.com>
2023-06-29 14:53:02 +0200

    Micro Update (Logo and bugs fix)

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Squale !", "author": "Lucy" },
+            { "message": "Vive les Pallas Cat !", "author": "Miro" },
+            { "message": "Hé Yo !", "author": "Lollia" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
